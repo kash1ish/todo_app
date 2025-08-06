a59cce1b97284e17bc5dafd0f014d162c9708a51
kash1ish <kash1ish2114@gmail.com>
2025-08-06 20:17:55 +0530

    Added

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
@@ -7,10 +8,25 @@ app.get("/todos",(req,res)=>{
 
 })
 app.post("/todo",(req,res)=>{
-    
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You have sent wrong inputs"
+        })
+        return;
+    }
+    //put it in mongodb
 })
 app.put("/completed",(req,res)=>{
-    
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You have sent wrong inputs"
+        })
+        return;
+    } 
 })
 
 
